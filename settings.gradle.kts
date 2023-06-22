666be1e4f27a14979a14c7479900befff8ab9a5c
Orlaando <orla3288@colorado.edu>
2023-06-22 17:20:03 -0600

    initial work for porting to fabric

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,9 @@
 rootProject.name = "PlotSquared"
 
-include("Core", "Bukkit")
+include("Core", "Bukkit", "Fabric")
 
 project(":Core").name = "plotsquared-core"
 project(":Bukkit").name = "plotsquared-bukkit"
+project(":Fabric").name = "plotsquared-fabric"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
